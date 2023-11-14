1628d618fe7635ab292b6ba9c69ee2a2b84650ad
fahizp <fahizp3@gmail.com>
2023-11-14 10:42:58 +0530

    production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Specify the output directory for the build
+    chunkSizeWarningLimit: 1024, // Adjust the chunk size warning limit if needed
+  },
+});
